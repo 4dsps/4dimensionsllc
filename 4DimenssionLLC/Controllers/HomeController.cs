f0018228d6ddb07a2ddbb5e18245deb1a8921ed7
BlazorT <info@blazortech.com>
2024-07-31 16:48:51 +0500

    Add project files.

---
+++ 4DimenssionLLC/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using _4DimenssionLLC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net.Mail;
+
+namespace _4DimenssionLLC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+		[HttpPost]
+		public IActionResult SendEmail(string recipient,  string body)
+		{
+			try
+			{				
+			
+				using (var message = new MailMessage())
+				{
+					message.From = new MailAddress("admin@4dsps.com");
+					message.To.Add("info@dimensionsllc.com");
+					message.Subject = "";
+					message.Body = body;
+
+					using (var client = new SmtpClient("email-smtp.us-east-1.amazonaws.com"))
+					{
+						//client.UseDefaultCredentials = false;
+						client.Credentials = new System.Net.NetworkCredential("AKIAZAN35G64ZK35DH7B", "BJr/bO8pFdND+Ef0LAxkjfFIbdA3Ms72/fU+G9nSLS0F");
+						client.EnableSsl = true;
+						client.Port = 587;
+						//EnableSsl = true,
+						client.DeliveryMethod = SmtpDeliveryMethod.Network;
+						client.UseDefaultCredentials = false;
+						client.Send(message);
+					}
+				}
+
+				return Ok("Email sent successfully!");
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"Failed to send email: {ex.Message}");
+			}
+		}
+		public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
