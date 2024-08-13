75da5ecf28f43983d78f7044ba38bf079ba756d7
arshad172 <arshad.ali@blazortech.com>
2024-08-13 11:52:22 +0500

    new

--- 4DimenssionLLC/obj/Debug/net8.0/4DimensionsLLC.AssemblyInfo.cs
+++ 4DimenssionLLC/obj/Debug/net8.0/4DimensionsLLC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4DimensionsLLC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba3d1514a407dc342659e215d4c2a3d911df96cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6fa033b1ee960ba7bacdb53caf832954f43e1ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("4DimensionsLLC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4DimensionsLLC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
