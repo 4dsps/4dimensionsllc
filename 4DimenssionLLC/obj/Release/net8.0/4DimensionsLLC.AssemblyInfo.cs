4f1f198f28147d19f3bd59303ca933d0de264653
4dsps <zahid.nawaz@blazortech.com>
2024-08-13 18:38:25 +0500

    commit of code

--- 4DimenssionLLC/obj/Release/net8.0/4DimensionsLLC.AssemblyInfo.cs
+++ 4DimenssionLLC/obj/Release/net8.0/4DimensionsLLC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4DimensionsLLC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac4a4feea78e19f73d371174546f873bea69ddff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c14b2a80a520c304277e84e3875a71430253a561")]
 [assembly: System.Reflection.AssemblyProductAttribute("4DimensionsLLC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4DimensionsLLC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
