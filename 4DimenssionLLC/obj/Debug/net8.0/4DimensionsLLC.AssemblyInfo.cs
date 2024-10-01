10eb0ab5c2792ffdf530369b1d12153200ed245c
arshad172 <arshad.ali@blazortech.com>
2024-10-01 15:54:45 +0500

    new changes

--- 4DimenssionLLC/obj/Debug/net8.0/4DimensionsLLC.AssemblyInfo.cs
+++ 4DimenssionLLC/obj/Debug/net8.0/4DimensionsLLC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4DimensionsLLC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6925dcc247c6fcc0068ee168795a722bb3d49a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e4ccb76d5908814bcc4b8076d681c4e18cb9970")]
 [assembly: System.Reflection.AssemblyProductAttribute("4DimensionsLLC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4DimensionsLLC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 4DimenssionLLC/wwwroot/js/site.js
+++ 4DimenssionLLC/wwwroot/js/site.js
@@ -27,17 +27,17 @@ const data = {
         },
         {
             "id": 4,
-            "name": "Eleanor Crisp",
+            "name": "Mike Jordan",
             "role": "Admin",
             "quote": "Dad buried in landslide! Jubilant throngs fill streets! Stunned father inconsolable - demands recount!",
-            "img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample3.jpg"
+            "img": "images/adil.jpg"
         },
         {
             "id": 5,
             "name": "Arshad Ali",
             "role": "Operation Manager",
             "quote": "Wormwood : Calvin, how about you? Calvin : Hard to say ma'am. I think my cerebellum has just fused.",
-            "img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample5.jpg"
+            "img": "images/myImg.jpg"
         },
         {
             "id": 6,
@@ -47,30 +47,41 @@ const data = {
             "img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample6.jpg"
         }
     ],
-	"events": [
-		{
-			"id": 1,
-			"name": "Thanks Giving",
-			"remarks": "Exellent Service",
-			"pics": ["images/event2.jpg", ]
-		},
-		//{
-		//	"id": 2,
-		//	"name": "Beats Bash",
-		//	"remarks": "Exellent Work",
-		//	"pics": ["images/event3.jpg"]
-		//},
-		//{
-		//	"id": 3,
-		//	"name": "Feel Good Fair",
-		//	"remarks": "Leading the team",
-		//	"pics": ["images/event4.jfif"]
-		//},
-		//{
-		//	"id": 4,
-		//	"name": "Edge Expo",
-		//	"remarks": "Best Performance",
-		//	"pics": ["images/event5.jpg"]
-		//}
-	]
+	
+    "events": [
+        {
+            "id": 1,
+            "name": "Thanks Giving",
+            "remarks": "Exellent Service",
+            "pics": ["images/event2.jpg"],
+            "dateTime": "2024-08-17T14:00:00",  // Past date
+            "status": [1]
+        },
+        {
+            "id": 2,
+            "name": "Beats Bash",
+            "remarks": "Exellent Work",
+            "pics": ["images/event3.jpg"],
+            "dateTime": "2024-04-17T16:30:00",  // Past date
+            "status": [1]
+        },
+        {
+            "id": 3,
+            "name": "Feel Good Fair",
+            "remarks": "Leading the team",
+            "pics": ["images/event4.jfif"],
+            "dateTime": "2024-11-10T18:00:00",  // Future date
+            "status": [1]
+        },
+        {
+            "id": 4,
+            "name": "Edge Expo",
+            "remarks": "Best Performance",
+            "pics": ["images/event5.jpg"],
+            "dateTime": "2024-12-05T20:00:00",  // Future date
+            "status": [1]
+        }
+    ]
+
+
 };
